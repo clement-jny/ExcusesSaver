69a72e504e331226af81a489937eabf6b0feedb1
Clément Jaunay <clementjaunay@free.fr>
2025-04-23 21:11:50 +0200

    feat/init project

---
+++ ExcusesSaver/ExcusesSaverView.h
@@ -0,0 +1,12 @@
+//
+//  ExcusesSaverView.h
+//  ExcusesSaver
+//
+//  Created by Clément Jaunay on 23/04/2025.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ExcusesSaverView : ScreenSaverView
+
+@end
---
+++ ExcusesSaver/ExcusesSaverView.m
@@ -0,0 +1,51 @@
+//
+//  ExcusesSaverView.m
+//  ExcusesSaver
+//
+//  Created by Clément Jaunay on 23/04/2025.
+//
+
+#import "ExcusesSaverView.h"
+
+@implementation ExcusesSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
